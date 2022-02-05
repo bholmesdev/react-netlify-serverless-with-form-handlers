6d4313108ee89c8d6d5902953225eeed35781493
Holben888 <bholmesdev@gmail.com>
2022-02-05 16:46:35 -0500

    chore: vitest verbose to match Jest

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    include: ['tests/vitest/**']
+    include: ['tests/vitest/**'],
+    reporters: 'verbose',
   }
 })
