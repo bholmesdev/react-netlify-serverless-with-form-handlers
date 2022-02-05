67a50ad4bf1ea846c8f07db84f4ee8a0fcf2d738
Holben888 <bholmesdev@gmail.com>
2022-02-05 16:46:20 -0500

    chore: nasty Jest config for react tests

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  clearMocks: true,
+  coverageProvider: "v8",
+  moduleNameMapper: {
+    // simplest way to stub out CSS imports without failures
+    // this avoids needing to set up webpack, style-loader, etc
+    // see https://jestjs.io/docs/webpack#mocking-css-modules
+    "\\.(css)$": "identity-obj-proxy",
+  },
+  roots: [
+    "<rootDir>/tests/jest"
+  ],
+};
