898c0aee0f347a76eb518262b644ac74cc8bd326
bholmesdev <hey@bholmes.dev>
2022-12-08 10:33:24 -0500

    chore: remove dead app code

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import Form from './components/Form'
-import './App.css'
+import Form from './components/Form';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <header>
-        <h1>Serverless TS + React + Netlify!</h1>
-      </header>
-      <main>
-        <Form />
-      </main>
-    </>
-  )
+	return (
+		<>
+			<header>
+				<h1>Serverless TS + React + Netlify!</h1>
+			</header>
+			<main>
+				<Form />
+			</main>
+		</>
+	);
 }
 
-export default App
+export default App;
