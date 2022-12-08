f1e07a535662f54e3b957ba0714236c15e728ab4
bholmesdev <hey@bholmes.dev>
2022-12-08 11:26:14 -0500

    reset: remove deps for demo

--- jest.config.js
+++ jest.config.js
@@ -6,11 +6,5 @@
 module.exports = {
 	clearMocks: true,
 	coverageProvider: 'v8',
-	moduleNameMapper: {
-		// simplest way to stub out CSS imports without failures
-		// this avoids needing to set up webpack, style-loader, etc
-		// see https://jestjs.io/docs/webpack#mocking-css-modules
-		'\\.(css)$': 'identity-obj-proxy',
-	},
 	roots: ['<rootDir>/tests/jest', '<rootDir>/src'],
 };
