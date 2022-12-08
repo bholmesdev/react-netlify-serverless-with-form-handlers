07d2431f9afbe086d309a589ecc2ab3d0e58c145
bholmesdev <hey@bholmes.dev>
2022-12-08 08:16:17 -0500

    fix: assert port for ntl dev

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+  },
   test: {
     include: ['tests/vitest/**'],
     reporters: 'verbose',
